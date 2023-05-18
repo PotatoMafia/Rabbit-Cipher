2589d3058b710db5f1ba77e08f6293d7c7362b59
akuhach <01184770@pw.edu.pl>
2023-05-18 17:47:11 +0200

    Zmiena typa dannych

--- RabbitCipher.java
+++ RabbitCipher.java
@@ -1,10 +1,10 @@
 public class RabbitCipher {
-    int[] key = new int[128];
-    int[] IV = new int[64];
-    int[][] subkeys = new int[8][16];
-    int[][] subIVEven = new int[4][32];
-    int[][] subIVOdd = new int[4][16];
-    int[][] counter = new int[7][4];
+    String key; // String key = "1111111111111111111111111110110111111111111111111111111111011011111111111111111111111111101101111111111111111111111111110110";
+    String IV;
+    String[] subkeys = new String[8];
+    String[] subIVEven = new String[2];
+    String[] subIVOdd = new String[4];
+    String[][] counter = new String[7][4];
     int[][] states = new int[7][4];
     private final int[] A = new int[]
             {
@@ -12,12 +12,15 @@ public class RabbitCipher {
                     0xD34D34D3, 0x34D34D34, 0x4D34D34D, 0xD34D34D3
             };
 
+    //concatenation of two bit sequences
+    public static String concatenateBitSequences(String sequence1, String sequence2) {
+        return sequence1 + sequence2;
+    }
+
+
     //creating subkeys
     //TODO: (@ksiepiet) Create method for subkeys
     protected void setSubKeys() {
-        int[][] subkeys = new int[8][16];
-        //....
-        this.subkeys = subkeys;
     }
 
     //TODO: (@ksiepiet) Create method for x_j,0[8]
@@ -38,19 +41,26 @@ public class RabbitCipher {
     //creating subIV
     //TODO: (@akuhach) Create method for subIV
     protected void setSubIV() {
-        int[][] subIVEven = new int[4][32];
-        int[][] subIVOdd = new int[4][16];
-        //....
-        this.subIVEven = subIVEven;
-        this.subIVOdd = subIVOdd;
+        subIVEven[0] = Integer.toBinaryString(Integer.parseInt(IV.substring(0, 32), 2));
+        subIVEven[1] = Integer.toBinaryString(Integer.parseInt(IV.substring(32, 64), 2));
+        subIVOdd[0] = Integer.toBinaryString(Integer.parseInt(IV.substring(0, 16), 2));
+        subIVOdd[1] = Integer.toBinaryString(Integer.parseInt(IV.substring(16, 32), 2));
+        subIVOdd[2] = Integer.toBinaryString(Integer.parseInt(IV.substring(32, 48), 2));
+        subIVOdd[3] = Integer.toBinaryString(Integer.parseInt(IV.substring(48, 64), 2));
+
     }
 
     //TODO: (@akuhach) Create method for c_j,0[8]
     protected void createCounterVariablesJZero() {
         for (int i = 0; i < counter.length; i++) {
             if (i % 2 == 0) {
+                int j1 = (i + 4) % 8;
+                int j2 = (i + 5) % 8;
+                counter[i][0] =concatenateBitSequences(subkeys[j1], subkeys[j2]);
                 //counter[i][0] =;
             } else {
+                int j = (i + 1) % 8;
+                counter[i][0] =concatenateBitSequences(subkeys[i], subkeys[j]);
                 // counter[i][0] =;
             }
         }
@@ -74,6 +84,9 @@ public class RabbitCipher {
 
     }
 
+     private void createF(){
+
+     }
     //TODO: (@akuhach) Create method for IV setup scheme
     protected void IVSetupScheme() {
         //....
