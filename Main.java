207fecbef7db060b588f568e06dc8ddc6cca5d35
akuhach <01184770@pw.edu.pl>
2023-06-07 17:42:06 +0200

    Full code version

--- Main.java
+++ Main.java
@@ -1,12 +1,37 @@
+import java.util.Arrays;
+
+
+
 public class Main {
     public static void main(String[] args) {
-        String key = "1111111111111111111111111110110111111111111111111111111111011011111111111111111111111111101101111111111111111111111111110110";
-        String IV = "1111111111111111111111111110110111111111111111111111111111011011111111111111111111111111101101111111111111111111111111110110";
-        RabbitCipher rabbitCipher = new RabbitCipher(key,IV);
-        String test1 = "abc";
-//        byte[] t = test1.getBytes();
-//        for(byte p : t){
-//            System.out.println(p);
-//        }
+        String inputString = "Hello, World!";
+        String key = "mysecretkey12345";
+        byte[] iv = { (byte)0x12, (byte)0x34, (byte)0x56, (byte)0x78, (byte) 0x90, (byte) 0xAB, (byte) 0xCD, (byte) 0xEF };
+
+        RabbitCipher rabbitCipher = new RabbitCipher();
+
+        // Set up the key and IV
+        rabbitCipher.setupKey(key.getBytes());
+        rabbitCipher.setupIV(iv);
+
+        // Encrypt the string
+        byte[] inputBytes = inputString.getBytes();
+        byte[] encryptedBytes = rabbitCipher.crypt(inputBytes);
+
+        // Print the encrypted bytes
+        System.out.println(Arrays.toString(encryptedBytes));
+
+        // Decrypt the bytes
+        rabbitCipher.reset();
+        rabbitCipher.setupKey(key.getBytes());
+        rabbitCipher.setupIV(iv);
+
+        byte[] decryptedBytes = rabbitCipher.crypt(encryptedBytes);
+
+        // Print the decrypted string
+        String decryptedString = new String(decryptedBytes);
+        System.out.println(decryptedString);
     }
+
+
 }
--- RabbitCipher.java
+++ RabbitCipher.java
@@ -1,150 +1,245 @@
-import java.math.BigInteger;
+import java.util.Arrays;
 
 public class RabbitCipher {
-    String key; // String key = "1111111111111111111111111110110111111111111111111111111111011011111111111111111111111111101101111111111111111111111111110110";
-    String IV;
-    String[] subkeys = new String[8];
-    String[] subIVEven = new String[2];
-    String[] subIVOdd = new String[4];
-    String[][] counters = new String[7][4];
-    String[][] states = new String[7][4];
-    private final BigInteger[] a = {
-            new BigInteger("4D34D34D", 16), new BigInteger("D34D34D3", 16),
-            new BigInteger("34D34D34", 16), new BigInteger("4D34D34D", 16),
-            new BigInteger("D34D34D3", 16), new BigInteger("34D34D34", 16),
-            new BigInteger("4D34D34D", 16), new BigInteger("D34D34D3", 16)
-    };
-
-    private static final int[] f = new int[8];
-    private static final BigInteger MOD = new BigInteger("2").pow(32);
-    RabbitCipher(String key, String IV){
-        this.key = key;
-        this.IV = IV;
+    //określa długość klucza strumienia (16 bajtów)
+    private static final int KEYSTREAM_LENGTH = 16;
+    //tablica zawierająca stałe wartości używane w algorytmie
+    private static final int[] a = new int[]
+            {
+                    0x4D34D34D, 0xD34D34D3,
+                    0x34D34D34, 0x4D34D34D, 0xD34D34D3,
+                    0x34D34D34, 0x4D34D34D,
+                    0xD34D34D3
+            };
+    private static final int rotl(final int value, final
+    int shift)
+    {
+        return value << shift | value >>> 32 - shift;
     }
-
-    //concatenation of two bit sequences
-    public static String concatenateBitSequences(String sequence1, String sequence2) {
-        return sequence1 + sequence2;
+    private final int[] setofX = new int[8];
+    private final int[] setofC = new int[8];
+    private byte b;
+    //to indeks klucza strumienia
+    private int keyindex = 0;
+    //tablica przechowująca wygenerowany klucz strumienia
+    private byte[] keystream = null;
+    public RabbitCipher()
+    {
+        b = 0;
     }
-
-    private String xorStrings(String s1, String s2) {
-        if (s1.length() != s2.length()) {
-            throw new IllegalArgumentException("Strings must be the same length");
-        }
-
-        StringBuilder result = new StringBuilder();
-        for (int i = 0; i < s1.length(); i++) {
-            char c1 = s1.charAt(i);
-            char c2 = s2.charAt(i);
-            if (c1 != '0' && c1 != '1' || c2 != '0' && c2 != '1') {
-                throw new IllegalArgumentException("Strings must have '0' or '1'");
+    //Dekoduje podany bajt byt przy użyciu metody crypt i zwraca odkodowany znak (typu char).
+    public char decryptChar(byte byt)
+    {
+        byte [] b = {byt};
+        //hack to get char from encrypted byte(array of lenght 1)
+        return (new String(crypt(b))).charAt(0);
+    }
+    /**
+     * Szyfruje lub deszyfruje przekazaną tablicę bajtów
+     * message przy użyciu klucza strumienia.
+     * Metoda iteruje po tablicy message,
+     * wykonując operację XOR z kolejnymi bajtami klucza strumienia.
+     * Jeśli klucz strumienia jest null lub indeks klucza osiągnął
+     * wartość KEYSTREAM_LENGTH, generowany jest nowy klucz strumienia
+     * poprzez wywołanie metody keyStream()
+     * */
+    public byte[] crypt(final byte[] message)
+    {
+        int index = 0;
+        while (index < message.length)
+        {
+            if (keystream == null || keyindex ==
+                    KEYSTREAM_LENGTH)
+            {
+                keystream = keyStream();
+                keyindex = 0;
             }
-            result.append(c1 == c2 ? '0' : '1');
-        }
-
-        return result.toString();
-    }
-    //creating subkeys
-    //TODO: (@ksiepiet) Create method for subkeys
-    protected void setSubKeys() {
-    }
-
-    //TODO: (@ksiepiet) Create method for x_j,0[8]
-    protected void createStateVariablesJZero() {
-    }
-
-    //TODO: (@ksiepiet) Create method for all states x[7][4]
-    protected void createAllStates() {
-
-    }
-
-
-    //creating subIV
-    //TODO: (@akuhach) Create method for subIV
-    protected void setSubIV() {
-        subIVEven[0] = Integer.toBinaryString(Integer.parseInt(IV.substring(0, 32), 2));
-        subIVEven[1] = Integer.toBinaryString(Integer.parseInt(IV.substring(32, 64), 2));
-        subIVOdd[0] = Integer.toBinaryString(Integer.parseInt(IV.substring(0, 16), 2));
-        subIVOdd[1] = Integer.toBinaryString(Integer.parseInt(IV.substring(16, 32), 2));
-        subIVOdd[2] = Integer.toBinaryString(Integer.parseInt(IV.substring(32, 48), 2));
-        subIVOdd[3] = Integer.toBinaryString(Integer.parseInt(IV.substring(48, 64), 2));
-
-    }
-
-    //TODO: (@akuhach) Create method for c_j,0[8]
-    protected void createCounterVariablesJZero() {
-        for (int i = 0; i < counters.length; i++) {
-            if (i % 2 == 0) {
-                int j1 = (i + 4) % 8;
-                int j2 = (i + 5) % 8;
-                counters[i][0] =concatenateBitSequences(subkeys[j1], subkeys[j2]);
-                //counter[i][0] =;
-            } else {
-                int j = (i + 1) % 8;
-                counters[i][0] =concatenateBitSequences(subkeys[i], subkeys[j]);
-                // counter[i][0] =;
+            for (; keyindex <
+                    KEYSTREAM_LENGTH && index <
+                    message.length; ++keyindex)
+            {
+                message[index++] ^=
+                        keystream[keyindex];
             }
         }
-        //....
-
+        return message;
     }
 
-    //TODO: (@akuhach) Create method for full counter system c_j,i
-    protected void createCounterSystem() {
-        for (int i = 0; i < counters.length; i++) {
-            for (int j = 0; j < counters[i].length; j++) {
-                        BigInteger counter = new BigInteger(counters[j][i], 2);
-                        BigInteger newValue = counter.add(a[j]).add(BigInteger.valueOf(f[(j+7)%8]));
-                counters[j][i+1] = String.format("%32s", newValue.mod(MOD).toString(2)).replace(' ', '0');
-                        f[j] = newValue.compareTo(MOD) >= 0 ? 1 : 0;
-                    }
-                }
-                // Display counter values
-                for (int i = 0; i < 7; i++) {
-                    for (int j = 0; j < 4; j++) {
-                        System.out.print(counters[i][j] + " ");
-                    }
-                    System.out.println();
-                }
+    /**
+     * Generuje klucz strumienia poprzez wykonanie kolejnego stanu algorytmu.
+     * Tworzona jest tablica s o rozmiarze 16 bajtów,
+     * która przechowuje wygenerowany klucz strumienia.
+     * Wartości klucza strumienia obliczane są na
+     * podstawie stanu setofX z wykorzystaniem operacji bitowych.
+     */
+    private byte[] keyStream()
+    {
+        nextState();
+        final byte[] s = new byte[16];
+        /*
+         * unroll
+         */
+        int x = this.setofX[6] ^ this.setofX[3] >>> 16 ^ this.setofX[1] << 16;
+        s[0] = (byte) (x >>> 24);
+        s[1] = (byte) (x >> 16);
+        s[2] = (byte) (x >> 8);
+        s[3] = (byte) x;
+        x = this.setofX[4] ^ this.setofX[1] >>> 16 ^ this.setofX[7] << 16;
+        s[4] = (byte) (x >>> 24);
+        s[5] = (byte) (x >> 16);
+        s[6] = (byte) (x >> 8);
+        s[7] = (byte) x;
+        x = this.setofX[2] ^ this.setofX[7] >>> 16 ^ this.setofX[5] << 16;
+        s[8] = (byte) (x >>> 24);
+        s[9] = (byte) (x >> 16);
+        s[10] = (byte) (x >> 8);
+        s[11] = (byte) x;
+        x = this.setofX[0] ^ this.setofX[5] >>> 16 ^ this.setofX[3] << 16;
+        s[12] = (byte) (x >>> 24);
+        s[13] = (byte) (x >> 16);
+        s[14] = (byte) (x >> 8);
+        s[15] = (byte) x;
+        return s;
+    }
+
+    /*
+     *ktualizuje stan algorytmu, przesuwając licznik i obliczając kolejny stan
+     * setofX na podstawie stanu setofC.
+     **/
+    private void nextState()
+    {
+        /*
+         * counter update
+         */
+        for (int j = 0; j < 8; ++j)
+        {
+            final long t = (setofC[j] & 0xFFFFFFFFL) +
+                    (a[j] & 0xFFFFFFFFL) + b;
+            b = (byte) (t >>> 32);
+            setofC[j] = (int) (t & 0xFFFFFFFF);
         }
-
-        //....
-
-    //TODO: (@akuhach) Create method for IV setup scheme
-    protected void IVSetupScheme() {
-        //....
-        modificationOfTheSetupScheme();
-    }
-
-    //TODO: (@akuhach) Create method for modifications
-    protected void modificationOfTheSetupScheme() {
-        remakeOfCounterVariables();
-        createCounterSystem();
-    }
-
-    private void remakeOfCounterVariables() {
-        for(int i = 0; i < counters.length; i++){
-            int j = (i+4) % 8;
-            counters[i][4] = xorStrings(counters[i][4], states[j][4]);
+        /*
+         * next state function
+         */
+        final int G[] = new int[8];
+        for (int j = 0; j < 8; ++j)
+        {
+// TODO: reduce this to use 32 bits only
+            long t = setofX[j] + setofC[j] & 0xFFFFFFFFL;
+            G[j] = (int) ((t *= t) ^ t >>> 32);
         }
+        /*
+         * unroll
+         */
+        setofX[0] = G[0] + rotl(G[7], 16) + rotl(G[6], 16);
+        setofX[1] = G[1] + rotl(G[0], 8) + G[7];
+        setofX[2] = G[2] + rotl(G[1], 16) + rotl(G[0], 16);
+        setofX[3] = G[3] + rotl(G[2], 8) + G[1];
+        setofX[4] = G[4] + rotl(G[3], 16) + rotl(G[2], 16);
+        setofX[5] = G[5] + rotl(G[4], 8) + G[3];
+        setofX[6] = G[6] + rotl(G[5], 16) + rotl(G[4], 16);
+        setofX[7] = G[7] + rotl(G[6], 8) + G[5];
     }
-
-    //TODO: Create extraction function
-    protected void extractionFunction() {
-
+    /**
+     *Czyści wszystkie wewnętrzne dane algorytmu Rabbit,
+     * umożliwiając ustawienie nowego klucza.
+     */
+    public void reset()
+    {
+        b = 0;
+        keyindex = 0;
+        keystream = null;
+        Arrays.fill(setofX, 0);
+        Arrays.fill(setofC, 0);
     }
-
-    //TODO: Create encryption function
-    protected void encryptionFunction() {
-
+    /**
+     * @param IV An array of 8 bytes
+     */
+    public void setupIV(final byte[] IV)
+    {
+        short[] sIV = new short[IV.length >> 1];
+        for (int i = 0; i < sIV.length; ++i)
+        {
+            sIV[i] = (short) ((IV[i << 1] << 8) | IV[(2
+                    << 1) + 1]);
+        }
+        setupIV(sIV);
     }
-
-    //TODO: Create decryption function
-    protected void decryptionFunction() {
-
+    /**
+     * * @param iv array of 4 short values
+     */
+    public void setupIV(final short[] iv)
+    {
+        /*
+         * unroll
+         */
+        setofC[0] ^= iv[1] << 16 | iv[0] & 0xFFFF;
+        setofC[1] ^= iv[3] << 16 | iv[1] & 0xFFFF;
+        setofC[2] ^= iv[3] << 16 | iv[2] & 0xFFFF;
+        setofC[3] ^= iv[2] << 16 | iv[0] & 0xFFFF;
+        setofC[4] ^= iv[1] << 16 | iv[0] & 0xFFFF;
+        setofC[5] ^= iv[3] << 16 | iv[1] & 0xFFFF;
+        setofC[6] ^= iv[3] << 16 | iv[2] & 0xFFFF;
+        setofC[7] ^= iv[2] << 16 | iv[0] & 0xFFFF;
+        nextState();
+        nextState();
+        nextState();
+        nextState();
     }
-
-    protected void run(){
-
+    /**
+     * @param key An array of 16 bytes
+     */
+    public void setupKey(final byte[] key)
+    {
+        short[] sKey = new short[key.length >> 1];
+        for (int i = 0; i < sKey.length; ++i)
+        {
+            sKey[i] = (short) ((key[i << 1] << 8) |
+                    key[(2 << 1) + 1]);
+        }
+        setupKey(sKey);
+    }
+    /**
+     * @param key An array of 8 short values
+     */
+    public void setupKey(final short[] key)
+    {
+        /*
+         * unroll
+         */
+        setofX[0] = key[1] << 16 | key[0] & 0xFFFF;
+        setofX[1] = key[6] << 16 | key[5] & 0xFFFF;
+        setofX[2] = key[3] << 16 | key[2] & 0xFFFF;
+        setofX[3] = key[0] << 16 | key[7] & 0xFFFF;
+        setofX[4] = key[5] << 16 | key[4] & 0xFFFF;
+        setofX[5] = key[2] << 16 | key[1] & 0xFFFF;
+        setofX[6] = key[7] << 16 | key[6] & 0xFFFF;
+        setofX[7] = key[4] << 16 | key[3] & 0xFFFF;
+        /*
+         * unroll
+         */
+        setofC[0] = key[4] << 16 | key[5] & 0xFFFF;
+        setofC[1] = key[1] << 16 | key[2] & 0xFFFF;
+        setofC[2] = key[6] << 16 | key[7] & 0xFFFF;
+        setofC[3] = key[3] << 16 | key[4] & 0xFFFF;
+        setofC[4] = key[0] << 16 | key[1] & 0xFFFF;
+        setofC[5] = key[5] << 16 | key[6] & 0xFFFF;
+        setofC[6] = key[2] << 16 | key[3] & 0xFFFF;
+        setofC[7] = key[7] << 16 | key[0] & 0xFFFF;
+        nextState();
+        nextState();
+        nextState();
+        nextState();
+        /*
+         * unroll
+         */
+        setofC[0] ^= setofX[4];
+        setofC[1] ^= setofX[5];
+        setofC[2] ^= setofX[6];
+        setofC[3] ^= setofX[7];
+        setofC[4] ^= setofX[0];
+        setofC[5] ^= setofX[1];
+        setofC[6] ^= setofX[2];
+        setofC[7] ^= setofX[3];
     }
 }
